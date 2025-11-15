5c36fb5c9f5dec735a1c11c3e3589b1cf69668f8
chayaacscollege-maker <chayaacscollege@gmail.com>
2025-11-15 10:46:59 +0530

    Add files via upload

---
+++ nandgate.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    04:30:13 12/08/2023 
+// Design Name: 
+// Module Name:    nandgate 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module nandgate(
+    input 0,
+    input 1,
+    output 0
+    );
+
+
+endmodule
